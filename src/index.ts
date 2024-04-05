99ee3ac9510916fe00b1147cae9b9f5733ae0054
Ante Schwender <a@b.c>
2024-04-05 11:03:21 +0200

    highcharts cdn version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { staticPlugin } from "@elysiajs/static";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(staticPlugin()).get("/", Bun.file("src/index.html")).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
